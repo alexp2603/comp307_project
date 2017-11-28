90c6d0d6de1adc834aef642185928e390aa5b9d9
LingRanLi <33789919+LingRanLi@users.noreply.github.com>
2017-11-28 13:30:37 -0500

    Formatting

--- book.php
+++ book.php
@@ -1,49 +1,65 @@
+<!DOCTYPE html>
+<html>
+ 
+<style>
+label {
+	padding-bottom: 12px;
+	padding-right: 10px;
+}
+</style>
+
 <?php
 
 include("includes/head.html");
 include("includes/header.php");
 
-
-$id_tutor = $_GET['id'];
-$course = $_GET['course'];
-$tutorname = $_GET['tutorname'];
-
-$connect = mysqli_connect("localhost", "root", "", "tutor");
-if (!$connect) {
-	echo("Connection failed: " . mysqli_connect_error());
-};
-
-$query = "SELECT STUDENT_NAME from students WHERE STUDENT_ID = $id_tutor";
-
-$result_TUTORNAME = mysqli_query($connect, $query);
-
-while($row = mysqli_fetch_array($result_TUTORNAME))
-{
-	$tutorname = $row['STUDENT_NAME'];
-}
-
-
-echo("<h2>Book a session of ".$course." course tutoring with ".$tutorname."!</h2>");
-
-echo("<form action='php/updateengagement.php?course=$course&id=$id_tutor&tutor_name=$tutorname' method='post'>");
 ?>
 
-<html>
-		<label>Date and time: </label><input type="datetime-local" name="datetime">
-		</br>
-		<label>Location: </label><input type="text" name="location">
-		</br>
-		<label>Duration: </label><input type="text" name="duration">
-		</br>
-		<label>Fee: </label><input type="text" name="fee">
-		</br>
-		<input type="submit" value="Book!">
-	</form>
+<div class="container">
+	<?php
 
-</html>
+	$id_tutor = $_GET['id'];
+	$course = $_GET['course'];
+	$tutorname = $_GET['tutorname'];
 
+	$connect = mysqli_connect("localhost", "root", "", "tutor");
+	if (!$connect) {
+		echo("Connection failed: " . mysqli_connect_error());
+	};
+
+	$query = "SELECT STUDENT_NAME from students WHERE STUDENT_ID = $id_tutor";
+
+	$result_TUTORNAME = mysqli_query($connect, $query);
+
+	while($row = mysqli_fetch_array($result_TUTORNAME))
+	{
+		$tutorname = $row['STUDENT_NAME'];
+	}
+
+
+	echo("<h2>Book a session of ".$course." course tutoring with ".$tutorname."!</h2>");
+
+	echo("<form action='php/updateengagement.php?course=$course&id=$id_tutor&tutor_name=$tutorname' method='post'>");
+	?>
+
+	<html>
+			<label>Date and time: </label><input type="datetime-local" name="datetime">
+			</br>
+			<label>Location: </label><input type="text" name="location">
+			</br>
+			<label>Duration: </label><input type="text" name="duration">
+			</br>
+			<label>Fee: </label><input type="text" name="fee">
+			</br>
+			<input type="submit" value="Book!">
+		</form>
+
+	</html>
+</div>
 
 
 <?php
 	include("includes/footer.php");
 ?> 
+
+</html>
--- showavailabletutors.php
+++ showavailabletutors.php
@@ -1,52 +1,68 @@
+<!DOCTYPE html>
+<html>
+<style type="text/css">
+    table{
+        border-collapse: separate;
+        border-spacing: 10px; /* Apply cell spacing */
+    }
+    table, th, td{
+        border: 1px solid #666;
+    }
+    table th, table td{
+        padding: 5px; /* Apply cell padding */
+    }
+</style>
+
 <?php
 
 include("includes/head.html");
 include("includes/header.php");
 ?>
 
-<html>
 
+<div class="container">
+            <?php
+
+                $course = $_POST['course'];
+                echo("<h2>Available $course Tutors</h2>");
+
+            	$connect = mysqli_connect("localhost", "root", "", "tutor");
+            	if (!$connect) {
+            		echo("Connection failed: " . mysqli_connect_error());
+            	};
+
+            	$query = mysqli_query($connect, "SELECT STUDENT_ID, STUDENT_NAME, STUDENT_EMAIL, STUDENT_YEAROFSTUDY, STUDENT_PHONE FROM students WHERE STUDENT_COURSE1 = '$course' OR STUDENT_COURSE2 = '$course' OR STUDENT_COURSE3 = '$course' OR STUDENT_COURSE4 = '$course' OR STUDENT_COURSE5 = '$course' ");
+
+            	if(mysqli_num_rows($query)>0)
+                {
+                    echo("<table cellspacing='10'><tr><th>Tutor name</th><th>Year of Study</th><th>Tutor Phone</th><th>Tutor E-Mail</th><th>Book An Engagement!</th></tr>");
+
+                    while($row = mysqli_fetch_array($query))
+                    {
+                    	$id = $row['STUDENT_ID'];
+                        $tutorname = $row['STUDENT_NAME'];
+                    	echo("<tr>");
+                    	echo("<th>".$row['STUDENT_NAME']."</th>");
+                    	echo("<th>".$row['STUDENT_YEAROFSTUDY']."</th>");
+                    	echo("<th>".$row['STUDENT_PHONE']."</th>");
+                    	echo("<th>".$row['STUDENT_EMAIL']."</th>");
+                    	echo("<th><a href='book.php?id=$id&course=$course&tutorname=$tutorname'>Book an engagement with ".$row['STUDENT_NAME']."</a></th>");
+                    	echo("</tr>");
+                    }
+                    echo("</table>");
+                }
+                else
+                {
+                    echo("Oops! It seems no one is tutoring for that course!");
+                }
+             ?>
+</div>
 
 <?php
-	$course = $_POST['course'];
-	echo("<h2>Available $course tutors</h2>");
-
-	$connect = mysqli_connect("localhost", "root", "", "tutor");
-	if (!$connect) {
-		echo("Connection failed: " . mysqli_connect_error());
-	};
-
-	$query = mysqli_query($connect, "SELECT STUDENT_ID, STUDENT_NAME, STUDENT_EMAIL, STUDENT_YEAROFSTUDY, STUDENT_PHONE FROM students WHERE STUDENT_COURSE1 = '$course' OR STUDENT_COURSE2 = '$course' OR STUDENT_COURSE3 = '$course' OR STUDENT_COURSE4 = '$course' OR STUDENT_COURSE5 = '$course' ");
-
-
-	if(mysqli_num_rows($query)>0)
-    {
-
-
-        echo("<table><tr><th>Tutor name</th><th>Year of Study</th><th>Tutor Phone</th><th>Tutor E-Mail</th><th>Book An Engagement!</th></tr>");
-
-        while($row = mysqli_fetch_array($query))
-        {
-        	$id = $row['STUDENT_ID'];
-            $tutorname = $row['STUDENT_NAME'];
-        	echo("<tr>");
-        	echo("<th>".$row['STUDENT_NAME']."</th>");
-        	echo("<th>".$row['STUDENT_YEAROFSTUDY']."</th>");
-        	echo("<th>".$row['STUDENT_PHONE']."</th>");
-        	echo("<th>".$row['STUDENT_EMAIL']."</th>");
-        	echo("<th><a href='book.php?id=$id&course=$course&tutorname=$tutorname'>Book an engagement with ".$row['STUDENT_NAME']."</a></th>");
-        	echo("</tr>");
-        }
-        echo("</table>");
-    }
-    else
-    {
-        echo("Oops! It seems no one is tutoring for that course!");
-    }
-
 
 include("includes/footer.php");
 ?>
+</html>
 
 
 
